5232a925625b62b30b9bae62388b8c5df614e62c
Hashemi Rafsan <rafsanhashemi@gmail.com>
2016-12-28 22:34:30 +0600

    80% work done

--- js/ajax.js
+++ js/ajax.js
@@ -1,29 +1,25 @@
-/*$(document).ready(function(){
-  $.ajax({
-    type:"GET",
-    url: "http://localhost/addon/index.php",
-    dataType: 'html',
-    data: ""
-  }).done(function(response) {
-    alert(response);
-  }).fail(function(data) {
-    $("#demo").html(data);
-    alert(data);
-    console.log(data);
-  });
-});
-*/
+
 $(document).ready(function(){
 $.ajax({
     type:"GET",
-    url: 'http://localhost/addon/index.php',                  //the script to call to get data
+    url: 'http://test.partyherenow.com/',                  //the script to call to get data
     data: "",                        //you can insert url argumnets here to pass to api.php
     crossDomain:true,
     async:true,                       //for example "id=5&parent=6"
     dataType: 'json'               //data format
   }).done(function(data){
-    var a = data[2];
-    $("#demo").html(a);
+    if((data instanceof Array) == true) {
+      for(var i = 0 ; i < 4 ; i++){
+        var a = data[i][1];
+        var b = data[i][2];
+        var c = data[i][3];
+        var d = data[i][4];
+        $("#cd-timeline").append("<div class='cd-timeline-block'>	<div class='cd-timeline-img cd-picture'><img src='img/cd-icon-picture.svg' alt='Picture'></div><div class='cd-timeline-content'><h2>"+a+"</h2><p>"+b+"</p><span class='cd-date'>"+d+"</span><span><p class='posted'> Posted by : "+c+"</p></span></div></div>");
+      }
+    }else{
+      $(".error").append(data);
+    }
+
   }).fail(function(data){
     console.log(data);
     $(".box").hide();
